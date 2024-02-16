92d7280405b0545e8729b1f0509adfe4f1f391ed
gabrieloakz <ferreiragabriel0193@gmail.com>
2024-02-16 19:52:23 +0100

    datatable

--- views/formulario.php
+++ views/formulario.php
@@ -22,6 +22,7 @@ $usuarios = $userController->listarUsuarios();
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <title>Formulário de Usuário</title>
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
+    <link rel="stylesheet" href="https://cdn.datatables.net/1.11.5/css/jquery.dataTables.min.css">
 </head>
 
 <body>
@@ -51,16 +52,38 @@ $usuarios = $userController->listarUsuarios();
 
         <!-- Lista de Usuários -->
         <h2 class="mt-4">Lista de Usuários</h2>
-        <ul>
-            <?php foreach ($usuarios as $usuario) : ?>
-                <li>
-                    <?php echo "{$usuario->nome} {$usuario->sobrenome}, {$usuario->idade} anos"; ?>
-                    <a href="editar.php?id=<?php echo $usuario->id; ?>">Editar</a>
-                    <a href="excluir.php?id=<?php echo $usuario->id; ?>">Excluir</a>
-                </li>
-            <?php endforeach; ?>
-        </ul>
+        <table id="myTable" class="display">
+            <thead>
+                <tr>
+                    <th>Nome</th>
+                    <th>Sobrenome</th>
+                    <th>Idade</th>
+                    <th>Ações</th>
+                </tr>
+            </thead>
+            <tbody>
+                <?php foreach ($usuarios as $usuario) : ?>
+                    <tr>
+                        <td><?php echo $usuario->nome; ?></td>
+                        <td><?php echo $usuario->sobrenome; ?></td>
+                        <td><?php echo $usuario->idade; ?></td>
+                        <td>
+                            <a href="editar.php?id=<?php echo $usuario->id; ?>">Editar</a>
+                            <a href="excluir.php?id=<?php echo $usuario->id; ?>">Excluir</a>
+                        </td>
+                    </tr>
+                <?php endforeach; ?>
+            </tbody>
+        </table>
     </div>
+
+    <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+    <script src="https://cdn.datatables.net/1.11.5/js/jquery.dataTables.min.js"></script>
+    <script>
+        $(document).ready(function() {
+            $('#myTable').DataTable();
+        });
+    </script>
 
 </body>
 
